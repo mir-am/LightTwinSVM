7447b25bb06258bec0dd9dfd6767350a6e42a75d
mir-am <amirmahmoudmir@gmail.com>
2018-05-28 20:31:53 +0430

    Unit test for LIBSVM data created.

--- src/test_program.py
+++ src/test_program.py
@@ -13,15 +13,14 @@ In this module, unit test is defined for checking the integrity of installation.
 """
 
 from eval_classifier import grid_search
-from dataproc import read_data
+from dataproc import read_data, read_libsvm
 import unittest
 
 
 class TestProgram(unittest.TestCase):
-    
+
     """
         It checks different functionalities of the program.    
-    
     """
     
     def __init__(self, *args, **kwargs):
@@ -68,7 +67,7 @@ class TestProgram(unittest.TestCase):
         grid_search(('CV', self.k_folds), 'RBF', self.training_data, self.labels, \
                     self.c_l_b, self.c_u_b, self.rbf_l_b, self.rbf_u_b, 1, self.output_file, \
                     self.result_path)
-        
+
     def test_RBF_ttsplit_gridsearch(self):
         
         """
@@ -78,9 +77,20 @@ class TestProgram(unittest.TestCase):
         grid_search(('t_t_split', self.train_set_size), 'RBF', self.training_data, self.labels, \
                     self.c_l_b, self.c_u_b, self.rbf_l_b, self.rbf_u_b, 1, self.output_file, \
                     self.result_path)
-    
- 
+
+    def test_LIBSVM_linear_CV_girdsearch(self):
+        
+        """
+        It checks linear kenel, CrossValidatiob and grid search with LIBSVM data
+        """
+        
+        X_train, y_train, file_name = read_libsvm("./dataset/heart.libsvm")
+
+        grid_search(('CV', self.k_folds), 'linear', X_train, y_train, self.c_l_b, \
+                    self.c_u_b, self.rbf_l_b, self.rbf_u_b, 1, self.output_file, \
+                    self.result_path)
+   
+
 if __name__ == '__main__':
-       
-    unittest.main()
     
+    unittest.main()
