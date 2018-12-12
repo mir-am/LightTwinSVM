5ecc044ec10e81f083846cc9b49d826e3d7bf0b7
mir-am <mir-am@hotmail.com>
2018-12-12 12:52:09 +0330

    Fixed plt_confusion_matrix error when matplotlib is not available. Also, Py 3.7 is added to TravisCI builds.

--- src/misc.py
+++ src/misc.py
@@ -16,7 +16,42 @@ import itertools
 import numpy as np
 
 try:
+    
     import matplotlib.pyplot as plt
+    
+    def plt_confusion_matrix(cm, classes, title='Confusion matrix',
+                         cmap=plt.cm.Blues):
+    
+        """
+        This function plots a confusion matrix
+        Input:
+            cm: A 2-d numpy array that contains confusion matrix
+            classes: Labels of classes
+            title(str): Title of confusion matrix at the top
+        """
+        
+        # Normalizing confusion matrix
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=90)
+        plt.yticks(tick_marks, classes, rotation=90)
+        
+        fmt = '.2f'
+        thresh = cm.max() / 2.0
+        
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            
+            plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", \
+                     color="white" if cm[i, j] > thresh else "black")
+            
+        plt.ylabel("True label")
+        plt.xlabel("Predicted label")
+        plt.tight_layout()
+    
 except ImportError:
     print("Couldn't import matplotlib package. However, this package is optional"
           " and required for plotting confusion matrix.")
@@ -60,38 +95,3 @@ def progress_bar_gs(iteration, total, e_time, accuracy, best_acc, prefix='', \
 
     if iteration == total:
         print()	
-
-
-def plt_confusion_matrix(cm, classes, title='Confusion matrix',
-                         cmap=plt.cm.Blues):
-    
-    """
-    This function plots a confusion matrix
-    Input:
-        cm: A 2-d numpy array that contains confusion matrix
-        classes: Labels of classes
-        title(str): Title of confusion matrix at the top
-    """
-    
-    # Normalizing confusion matrix
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=90)
-    plt.yticks(tick_marks, classes, rotation=90)
-    
-    fmt = '.2f'
-    thresh = cm.max() / 2.0
-    
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", \
-                 color="white" if cm[i, j] > thresh else "black")
-        
-    plt.ylabel("True label")
-    plt.xlabel("Predicted label")
-    plt.tight_layout()
-    
